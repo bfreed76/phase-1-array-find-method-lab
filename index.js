9008c1afce2c98f6b1d223e1e4c8018a2f976876
Brinton <brintonfoyreed@gmail.com>
2021-04-08 13:27:39 -0700

    commit tree 6dce974048877fbe0bd0550dce1b5533acef4f52

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+const superbowlWin = (array) => {
+    const found = array.find((i) => {
+        return i.result==="W";
+    })
+        if (found === undefined) {
+            return undefined;
+        } else { 
+            return found.year;
+}
+}
+
+// return found ? found.year: undefined
+
